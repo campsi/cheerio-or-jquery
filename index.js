3b1ad6cab3bbdba97e52cd7b83194c2b3152b955
romainbessugesmeusy <romainbessuges@gmail.com>
2016-03-10 14:57:32 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var isBrowser = require('is-browser');
+var extend;
+var cheerio;
+
+if (!isBrowser) {
+
+    cheerio = require('cheerio');
+    extend = require('extend');
+
+    ['on', 'off', 'bind'].forEach(function (methodName) {
+        cheerio.prototype[methodName] = new Function();
+    });
+
+    cheerio.prototype.prop = cheerio.prototype.attr;
+    cheerio.prototype.extend = extend;
+
+    cheerio.prototype.toggle = function (force) {
+        this.css('dislay', (force === false) ? 'none' : 'inherit');
+    };
+
+    cheerio.prototype.hide = function () {
+        this.toggle(false);
+    };
+
+    cheerio.prototype.show = function () {
+        this.toggle(true);
+    };
+}
+
+module.exports = (isBrowser) ? window.jQuery : require('cheerio').load('');
